c8c2ccd413a3cecf8e396fb39581a1b69499486d
Armand Guim <armandguim3@gmail.com>
2018-02-01 06:36:29 -0500

    fleshed out data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 	{
 		name: 'Elon Musk',
-		photo: 'http://static.businessinsider.sg/sites/2/2017/10/59ef5b0c909924d2008b6aeb.jpg'
+		photo: 'http://static.businessinsider.sg/sites/2/2017/10/59ef5b0c909924d2008b6aeb.jpg',
 		scores: [
 			'1',
 			'1',
@@ -14,11 +14,172 @@ var friendsArray = [
 			'1',
 			'1'
 		]
+	},
+	{
+		name: 'Steve Ballmer',
+		photo: 'https://lh3.googleusercontent.com/m4s7KYt2KLH3muyPAB9vKduibK_vZPkWQCXxFDjMCkSvfzupr9m9ZDbyDFfS56BG9xDYI9foCyALxCvopTg5PDZR1QsATO_wap3vGaPynJ9Rc_mPi2reuIbdlA',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Steve Jobs',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/1200px-Steve_Jobs_Headshot_2010-CROP.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bill Gates',
+		photo: 'https://pbs.twimg.com/profile_images/929933611754708992/ioSgz49P_400x400.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+{
+		name: 'Jeff Bezos',
+		photo: 'http://www.slate.com/content/dam/slate/articles/technology/technology/2017/07/170727_TECH_JeffBezos-RichestMan.jpg.CROP.promo-xlarge2.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Mark Zuckerberg',
+		photo: 'https://fortunedotcom.files.wordpress.com/2016/11/163292239.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+{
+		name: 'Shigeru Miyamoto',
+		photo: 'https://vignette.wikia.nocookie.net/nintendo/images/e/e6/Shigeru_Miyamoto.jpg/revision/latest?cb=20170517154724&path-prefix=en',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Porter Robinson',
+		photo: 'https://images.sk-static.com/images/media/profile_images/artists/3821916/huge_avatar',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+{
+		name: 'Neo',
+		photo: 'https://i0.wp.com/deepaknair.com/supercool/wp-content/uploads/2012/12/neomatrixbullets.jpg?resize=460%2C311',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Spock',
+		photo: 'https://uproxx.files.wordpress.com/2015/08/instagram-spock.png',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
 	}
 
+//THIS DATA STRUCTURE IS LEFT BLANK FOR LATER USE
+// {
+// 		name: '',
+// 		photo: '',
+// 		scores: [
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1',
+// 			'1'
+// 		]
+// 	}
 
 
 
+];
 
-
-]
+module.exports = friendsArray;
